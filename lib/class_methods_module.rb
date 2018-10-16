84337db46b130a6a21b383660bc5c01f3312cbfd
magic8baller <lturis@gmail.com>
2018-10-16 18:16:13 -0400

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,8 @@
+#to lend a module's methods to a class as class methods, we use the 'extend' keyword
+
+module MetaDancing
+
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
