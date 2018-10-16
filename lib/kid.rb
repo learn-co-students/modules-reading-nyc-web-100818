c4bda139fbeeb30fdd97421e8eda1714a06742f1
Veraprot <vera.protopopova@gmail.com>
2018-10-16 13:08:49 -0400

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,15 @@
-require_relative './dance_module.rb'
-require_relative './class_methods_module.rb'
+require_relative './dance_module.rb' #tells that we need to use this file 
+require_relative './class_methods_module.rb' #load this file too 
+require_relative './fancy_dance.rb'
+class Kid
+  # include Dance #include methods from Dance module 
+  # extend MetaDancing 
+
+  include FancyDance::InstanceMethods 
+  extend FancyDance::ClassMethods 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+end 
